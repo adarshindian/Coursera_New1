72b2d62ffb184917c28a2b1351b5bec6a527236a
Adarsh <adarshpro43@gmail.com>
2021-11-16 03:20:58 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent';
 import { Switch,Route,Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 class Main extends Component{
 
   constructor(props){
@@ -59,6 +60,7 @@ const DishWithId=({match})=>{
             <Route path="/hhome" component={HomePage}/> 
             <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
             <Route exact path="/contactus" component={Contact}/>
+            <Route exact path="/aboutus" component={()=><About leaders={this.state.leaders}/>}/>
             <Route path='/menu/:dishId' component={DishWithId}/>
             <Redirect to="/hhome"/>
           </Switch> 
