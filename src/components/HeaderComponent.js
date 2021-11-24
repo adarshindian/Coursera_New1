e3a2c3bbb772355fdae4d6835badae3757a4feb4
Adarsh <adarshpro43@gmail.com>
2021-11-25 01:48:32 +0530

    After Adding Bootstrap

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,76 +1,5 @@
-// import React, {Component} from 'react';
-// import {Jumbotron, Navbar,NavbarBrand,Nav,NavbarToggler,Collapse,NavItem} from 'reactstrap';
-// import {NavLink} from 'react-router-dom';
-// class Header extends Component{
-//   constructor(props)
-//   {
-//     super(props);
-//     this.toggleNav=this.toggleNav.bind(this);
-//     this.state={
-//       isNavOpen:false
-//     };
-   
-//   }
-//   toggleNav()
-//   {
-//     this.setState(
-//       {
-//         isNavOpen:!this.state.isNavOpen
-//       }
-//     )
-//   }
-//     render()
-//     {
-//         return(
-//             <>
-//                   <Navbar  dark expand="md">
-//             <div className="container">
-//             <NavbarToggler onClick={this.toggleNav}/>
-//               <NavbarBrand className="mr-auto" href="/" ><img src="assets/images/logo.png" height="30"width="41" alt="Ristorante Con Fusion"/></NavbarBrand>
-//               <Collapse isOpen={this.state.isNavOpen}>
-//               <Nav navbar>
-//                 <NavItem>
-//                   <NavLink className="nav-link" to ="/home">
-//                     <span className="fa fa-home fa-lg"></span>Home
-//                   </NavLink>
-//                 </NavItem>
-//                 <NavItem>
-//                   <NavLink className="nav-link" to ="/aboutus">
-//                     <span className="fa fa-info fa-lg"></span>About US
-//                   </NavLink>
-//                 </NavItem>
-//                 <NavItem>
-//                   <NavLink className="nav-link" to ="/menu">
-//                     <span className="fa fa-home fa-lg"></span>Menu
-//                   </NavLink>
-//                 </NavItem>
-//                 <NavItem>
-//                   <NavLink className="nav-link" to ="/home">
-//                     <span className="fa fa-address-card fa-lg"></span>Address
-//                   </NavLink>
-//                 </NavItem>
-//               </Nav>
-//               </Collapse>
-//             </div>
-//           </Navbar>
-//           <Jumbotron>
-//               <div className="container">
-//                   <div className="row row-header">
-//                     <div className="col-12 col-sm-6">
-//                         <h1>Ristorante  Con Fusion </h1>
-//                         <p>We take inspiration from the World's best cisines, and create a unique fusion experience. Our lipsmacking creaions will tickle your culinary senses! </p>
-//                     </div>
-//                   </div>
-//               </div>
-//           </Jumbotron>
-//             </>
-//         );
-//     }
-// }
-// export default Header;
 
-
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Modal,ModalHeader,ModalBody, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import React, {Component} from 'react';
 
@@ -78,16 +7,26 @@ class Header extends Component {
     constructor(props) {
         super(props);
     
-        this.toggleNav = this.toggleNav.bind(this);
+       
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOPen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
       }
 
       toggleNav() {
         this.setState({
           isNavOpen: !this.state.isNavOpen
         });
+      }
+      toggleModal(){
+this.setState(
+    {
+        isModalOPen:!this.state.isModalOPen
+    }
+)
       }
 
     render() {
@@ -112,6 +51,11 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/contactus' style={{color: 'white', textDecoration: 'none'}}><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Logi</Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -125,6 +69,14 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOPen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        Login
+                    </ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
